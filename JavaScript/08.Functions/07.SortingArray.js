c24178824000f4638961281e36a750897c78bc89
lllevski <mitkostoikov1988@gmail.com>
2016-06-26 21:36:41 +0300

    update after fixed tests

--- JavaScript/08.Functions/07.SortingArray.js
+++ JavaScript/08.Functions/07.SortingArray.js
@@ -1,7 +1,6 @@
 function sort(input) {
-    var arr = input[0].split('\n'),
-    n = +arr[0],
-    numbers = arr[1].split(' ').map(Number);
+    var n = +input[0],
+    numbers = input[1].split(' ').map(Number);
     
     if (numbers.length > 1) {
         console.log(numbers.sort(function (x ,y)
