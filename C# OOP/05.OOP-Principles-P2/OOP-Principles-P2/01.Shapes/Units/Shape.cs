874548facb9bc3fcab2c3378251ea90bfd02d94c
lllevski <mitkostoikov1988@gmail.com>
2016-06-26 18:21:28 +0300

    added oop-principles p2(without 3rd task)

---
+++ C# OOP/05.OOP-Principles-P2/OOP-Principles-P2/01.Shapes/Units/Shape.cs
@@ -0,0 +1,55 @@
+﻿namespace _01.Shapes.Units
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class Shape
+    {
+        private double width;
+        private double height;
+
+        public Shape(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width cannot be a negative number!");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height cannot be a negative number!");
+                }
+                this.height = value;
+            }
+        }
+
+        public abstract void CalculateSurface();
+
+    }
+}
---
+++ C# OOP/05.OOP-Principles-P2/OOP-Principles-P2/03.RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,13 @@
+﻿namespace _03.RangeExceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class InvalidRangeException<T> where T : IComparable<T>
+    {
+
+    }
+}
