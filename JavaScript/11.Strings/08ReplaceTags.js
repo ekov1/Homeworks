9322bc4fc48c3e5d9a641fc6fd8fbef4ab250aba
lllevski <mitkostoikov1988@gmail.com>
2016-07-01 02:01:35 +0300

    added 08ReplaceTags solution(time limit!!)

--- JavaScript/11.Strings/08ReplaceTags.js
+++ JavaScript/11.Strings/08ReplaceTags.js
@@ -1,9 +1,30 @@
 function replaceTags(args) {
-    var text = args[0];
+    var text = args[0],
+        content = /">(.*?)<\/a>/g;
 
-    text = text.replace(/<a href="/g, '[URL=');
-    text = text.replace(/">/g, ']');
-    text = text.replace(/<\/a>/g, '[/URL]');
+    var contArr = text.match(content),
+        contSubstr = "",
+        toPut = "",
+        aIndex = 0,
+        beforeTag = "";
+
+    for (var i = 0; i < contArr.length; i += 1) {
+
+        aIndex = text.indexOf('<a');
+
+        contSubstr = contArr[i].substring(2, contArr[i].length - 4);
+        toPut = '[' + contSubstr + '](';
+
+        beforeTag = text.substring(aIndex, aIndex + 9);
+        text = text.replace(beforeTag, toPut);
+        
+        aIndex = text.indexOf('<a', aIndex + 9);
+    }
+
+    text = text.replace(content, ')');
+    text = text.replace(/<\/a>/g, '');
 
     console.log(text);
 }
+
+replaceTags(['<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>']);
