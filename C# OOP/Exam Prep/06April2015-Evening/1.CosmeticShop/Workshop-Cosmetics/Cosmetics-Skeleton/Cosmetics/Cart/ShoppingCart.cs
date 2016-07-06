5795c235ec09db4bd4770a04b960ba23f0a74fb6
lllevski <mitkostoikov1988@gmail.com>
2016-07-06 22:25:01 +0300

    added exam prep folder

---
+++ C# OOP/Exam Prep/06April2015-Evening/1.CosmeticShop/Workshop-Cosmetics/Cosmetics-Skeleton/Cosmetics/Cart/ShoppingCart.cs
@@ -0,0 +1,56 @@
+﻿using Cosmetics.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Cart
+{
+    public class ShoppingCart
+    {
+        private ICollection<IProduct> productList;
+
+        public ShoppingCart()
+        {
+            this.ProductList = new List<IProduct>();
+        }
+
+        public ICollection<IProduct> ProductList
+        {
+            get { return this.productList; }
+            set { productList = value; }
+        }
+
+        public void AddProduct(IProduct product)
+        {
+            productList.Add(product);
+        }
+
+        public void RemoveProduct(IProduct product)
+        {
+            productList.Remove(product);
+        }
+
+        public bool ContainsProduct(IProduct product)
+        {
+            return productList.Contains(product);
+        }
+
+        public decimal TotalPrice()
+        {
+            decimal total = 0.00M;
+
+            if (productList.Count > 0)
+            {
+                foreach (var product in ProductList)
+                {
+                    total += product.Price;
+                }
+                return total;
+
+            }
+            return 0;
+        }
+    }
+}
---
+++ C# OOP/Exam Prep/06April2015-Evening/1.CosmeticShop/Workshop-Cosmetics/Cosmetics-Skeleton/Cosmetics/Products/Product.cs
@@ -0,0 +1,78 @@
+﻿namespace Cosmetics.Products
+{
+    using Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Common;
+
+    public abstract class Product : IProduct
+    {
+        private decimal price;
+        private string name;
+        private string brand;
+
+        public Product(string name, string brand, decimal price, GenderType gender)
+        {
+            this.Name = name;
+            this.Brand = brand;
+            this.Price = price;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                string form = String.Format(GlobalErrorMessages.InvalidStringLength, "Product name", 3, 10);
+                Validator.CheckIfStringLengthIsValid(value, 10, 3, form);
+                this.name = value;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return this.brand;
+            }
+            private set
+            {
+                string form = String.Format(GlobalErrorMessages.InvalidStringLength, "Product brand", 2, 10);
+                Validator.CheckIfStringLengthIsValid(value, 10, 2, form);
+                this.brand = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                this.price = value;
+            }
+        }
+
+        public GenderType Gender { get; set; }
+
+        public virtual string Print()
+        {
+            StringBuilder builder = new StringBuilder();
+            
+            builder.AppendLine(string.Format("- {0} - {1}:", this.Brand, this.Name));
+            builder.AppendLine(string.Format("  * Price: ${0}", this.Price));
+            builder.AppendLine(string.Format("  * For gender: {0}", this.Gender));
+
+            return builder.ToString().Trim();
+        }
+    }
+}
