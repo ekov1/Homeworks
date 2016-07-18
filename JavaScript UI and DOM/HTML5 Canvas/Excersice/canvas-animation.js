75a2c20463b5f235d0487a996d3ca5e581fbb209
lllevski <mitkostoikov1988@gmail.com>
2016-07-18 19:44:33 +0300

    added JS UI and DOM excersice

---
+++ JavaScript UI and DOM/HTML5 Canvas/Excersice/canvas-animation.js
@@ -0,0 +1,43 @@
+(function () {
+    var theCanvas = document.getElementById("the-canvas-animation");
+    var ctx = theCanvas.getContext("2d");
+
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgba(102, 25, 77, 0.7)';
+
+    function quadraticCurve(ctx, s1, s2, c) {
+
+        ctx.beginPath();
+        ctx.moveTo(s1.x, s1.y);
+        ctx.quadraticCurveTo(c.x, c.y, s2.x, s2.y);
+        ctx.stroke();
+    }
+
+    var s1 = {
+        x: 10,
+        y: 10
+    },
+        s2 = {
+            x: 500,
+            y: 10
+        },
+        i = 0;
+
+    function animation() {
+        if (i >= 50) {
+            ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+            ctx.moveTo(0, 0);
+            i = 0;
+        }
+
+        var c = {
+            x: i * 25,
+            y: 200
+        };
+        i += 1;
+        quadraticCurve(ctx, s1, s2, c);
+
+        requestAnimationFrame(animation);
+    }
+    animation();
+} ());
---
+++ JavaScript UI and DOM/HTML5 Canvas/Excersice/canvas-shapes.js
@@ -0,0 +1,49 @@
+(function () {
+    var canvas = document.getElementById('the-canvas-shapes'),
+        ctx = canvas.getContext('2d');
+    
+    ctx.strokeStyle = '#22545F';
+    ctx.fillStyle = '#90CAD7';
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(270, 220);
+    ctx.arc(200, 220, 70, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+
+    ctx.fillStyle = '#396693';
+    ctx.strokeStyle = '#262626';
+    ctx.save();
+
+    ctx.beginPath();
+    ctx.scale(2, 0.5);
+    ctx.moveTo(150, 350);
+    ctx.arc(100, 350, 50, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.restore();
+    ctx.beginPath();
+    ctx.moveTo(150, 160);
+    ctx.lineTo(150, 30);
+    ctx.lineTo(250, 30);
+    ctx.lineTo(250, 160);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.moveTo(150, 160);
+    ctx.quadraticCurveTo(200, 180, 250, 160);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.scale(2, 0.5); // TODO: Find easy way to position scaled objects!!!
+    
+    ctx.moveTo(125, 60);
+    ctx.arc(100, 60, 25, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+} ());
