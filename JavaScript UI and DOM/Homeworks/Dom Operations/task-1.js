fb37f4d3d609bce9f54a1779c06ef6441bdb0b35
lllevski <mitkostoikov1988@gmail.com>
2016-07-22 14:18:04 +0300

    small changes

--- JavaScript UI and DOM/Homeworks/Dom Operations/task-1.js
+++ JavaScript UI and DOM/Homeworks/Dom Operations/task-1.js
@@ -1,9 +1,27 @@
+/* 
+Create a function that takes an id or DOM element and an array of contents
+* if an id is provided, select the element
+* Add divs to the element
+  * Each div's content must be one of the items from the contents array
+* The function must remove all previous content from the DOM element provided
+* Throws if:
+  * The provided first parameter is neither string or existing DOM element
+  * The provided id does not select anything (there is no element that has such an id)
+  * Any of the function params is missing
+  * Any of the function params is not as described
+  * Any of the contents is neight `string` or `number`
+    * In that case, the content of the element **must not be** changed   
+*/
 function solve() {
 
     return function (element, contents) {
 
         var after,
-            toAppend = '';
+            toAppend = '',
+            len = contents.length,
+            div = document.createElement('div'),
+            divElement,
+            docFragment = document.createDocumentFragment();
 
         if (typeof element === 'string') {
             after = document.getElementById(element);
@@ -23,12 +41,12 @@ function solve() {
 
         after.innerHTML = '';
 
-        var divElement;
-
         for (var i = 0; i < contents.length; i += 1) {
-            divElement = document.createElement('div');
+            
+            divElement = div.cloneNode(true);
             divElement.innerHTML = contents[i];
-            after.appendChild(divElement);
+            docFragment.appendChild(divElement);
         }
+        after.appendChild(docFragment);
     };
 }
