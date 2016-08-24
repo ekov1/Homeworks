cf90105959f59d02e59461726f0362eb24a15068
lllevski <mitkostoikov1988@gmail.com>
2016-08-24 14:00:12 +0300

    update

--- JavaScript OOP/03. Closures-and-Scope/task-1.js
+++ JavaScript OOP/03. Closures-and-Scope/task-1.js
@@ -1,17 +1,36 @@
-var library = (function() {
-    var category;
+var library = (function () {
+    var category,
+        id = 0,
+        books = [],
+        categories = [];
 
 
-    function addNewBookToCategory(category) {
+    function addNewBookToCategory(book, category) {
+        book.id = ++id;
 
+        if (!category) {
+            category = [];
+        }
+
+        category.push(book);
+        // books.push(book);
+        return category;
     }
 
     function listAllBooks() {
+        if (!books) {
+            return null;
+        }
 
+// should add function to sort with other contidions
+        books.sort(function (a, b) {
+            return a.id - b.id;
+        });
+        return books;
     }
 
     function listAllCategories() {
-        
+
     }
 
 })();
