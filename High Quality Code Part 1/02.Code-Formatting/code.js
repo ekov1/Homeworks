308be5a4d608dad7788e3250aa2530fca3112890
lllevski <mitkostoikov1988@gmail.com>
2016-08-25 14:25:02 +0300

    update

--- High Quality Code Part 1/02.Code-Formatting/code.js
+++ High Quality Code Part 1/02.Code-Formatting/code.js
@@ -4,63 +4,88 @@ var pX = 0,
 	addScroll = false,
 	theLayer,
 	navigator,
-	event;
+	event,
+	NETSCAPE = 'Netscape';
 
 if (navigator.userAgent.indexOf('MSIE 5') > 0 ||
 	navigator.userAgent.indexOf('MSIE 6') > 0) {
     addScroll = true;
 }
 
+document.onmousemove = mouseMove;
+
 // Not sure which event this line is using
-if (navAppName === 'Netscape') {
+if (navAppName === NETSCAPE) {
     document.captureEvents(Event.MOUSEMOVE);
 }
 
-document.onmousemove(function mouseMove(ev) {
-	pX = ev.pageX - 5;
-	pY = ev.pageY;
+function mouseMove(ev) {
+    if (navAppName === NETSCAPE) {
+        pX = ev.pageX - 5;
+        pY = ev.pageY;
 
-	ShowToolTip();
-});
+    } else {
+        pX = event.x - 5;
+        pY = event.y;
+    }
+
+	if (document.layers.ToolTip.visibility === 'show' ||
+		document.all.ToolTip.style.visibility === 'visible') {
+		PopTip();
+	}
+}
 
 function ShowToolTip() {
-    if (navAppName === 'Netscape') {
 
-        theLayer = document.layers.ToolTip;
+	var layerLeft,
+		layerTop,
+		offsetLeft = 10,
+		offsetTop = 15,
+		offsetWidth = 150,
+		documentWidth;
 
-        if ((pX + 120) > window.innerWidth) {
-            pX = window.innerWidth - 150;
+	theLayer = document.layers.ToolTip || document.all.ToolTip;
+	documentWidth = window.innerWidth || document.body.clientWidth;
+
+	if (!theLayer) {
+		return;
+	}
+
+
+	//----------------------------------------------------------------------------
+    if (navAppName === NETSCAPE) {
+
+        if ((pX + 120) > documentWidth) {
+            pX = window.innerWidth - offsetWidth;
         }
 
-        theLayer.left = pX + 10;
-        theLayer.top = pY + 15;
+        theLayer.left = pX + offsetLeft;
+        theLayer.top = pY + offsetTop;
         theLayer.visibility = 'show';
 
     } else {
-        theLayer = document.all.ToolTip;
 
-        if (theLayer) {
-            pX = event.x - 5;
-            pY = event.y;
+		pX = event.x - 5;
+		pY = event.y;
 
-            if (addScroll) {
-                pX = pX + document.body.scrollLeft;
-                pY = pY + document.body.scrollTop;
-            }
-            if ((pX + 120) > document.body.clientWidth) {
-                pX = pX - 150;
-            }
+		if (addScroll) {
+			pX = pX + document.body.scrollLeft;
+			pY = pY + document.body.scrollTop;
+		}
+		if ((pX + 120) > documentWidth) {
+			pX = pX - offsetWidth;
+		}
 
-            theLayer.style.pixelLeft = pX + 10;
-            theLayer.style.pixelTop = pY + 15;
-            theLayer.style.visibility = 'visible';
-        }
+		theLayer.style.pixelLeft = pX + offsetLeft;
+		theLayer.style.pixelTop = pY + offsetTop;
+		theLayer.style.visibility = 'visible';
     }
+	//----------------------------------------------------------------------------
 }
 
 // ToolTip, menu1 or menu2 can be hidden with command
 function Hide(obj) {
-    if (navAppName === 'Netscape') {
+    if (navAppName === NETSCAPE) {
         document.layers[obj].visibility = 'hide';
     } else {
         document.all[obj].style.visibility = 'hidden';
@@ -69,12 +94,12 @@ function Hide(obj) {
 
 // Can show either menu1 or menu2
 function ShowMenu(menu) {
-    if (navAppName === 'Netscape') {
+    if (navAppName === NETSCAPE) {
 
         theLayer = document.layers[menu];
         theLayer.visibility = 'show';
     } else {
-		
+
         theLayer = document.all[menu];
         theLayer.style.visibility = 'visible';
     }
