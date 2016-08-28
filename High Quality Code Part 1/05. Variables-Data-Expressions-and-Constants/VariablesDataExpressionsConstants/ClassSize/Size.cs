7dd7a6e3e545b1f534ac482a4914450dc1fa634e
lllevski <mitkostoikov1988@gmail.com>
2016-08-28 14:45:37 +0300

    Variables, Data, Expressions and Constants Homework

---
+++ High Quality Code Part 1/05. Variables-Data-Expressions-and-Constants/VariablesDataExpressionsConstants/ClassSize/Size.cs
@@ -0,0 +1,82 @@
+﻿namespace ClassSize
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be equal or less than zero!");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be equal or less than zero!");
+                }
+                this.height = value;
+            }
+        }
+
+        /// <summary>
+        /// Rotates a size by a rotation angle
+        /// </summary>
+        /// <param name="size">The initial size</param>
+        /// <param name="rotationAngle">The rotation angle, which rotates the size</param>
+        /// <returns>Returns the rotated size</returns>
+        public static Size GetRotatedSize(Size size, double rotationAngle)
+        {
+            double rotationAngleSinus = Math.Abs(Math.Sin(rotationAngle));
+            double rotationAngleCosinus = Math.Abs(Math.Cos(rotationAngle));
+
+            double widthAfterRotation = rotationAngleCosinus * size.Width + rotationAngleSinus * size.Height;
+            double heightAfterRotation = rotationAngleSinus * size.Width + rotationAngleCosinus * size.Height;
+
+            var newSize = new Size(widthAfterRotation, heightAfterRotation);
+            return newSize;
+        }
+
+        public override string ToString()
+        {
+            var width = this.Width.ToString("0.00");
+            var height = this.Height.ToString("0.00");
+
+            var toString = String.Format($"The size has width {width} and height {height} !");
+
+            return toString;
+        }
+    }
+}
---
+++ High Quality Code Part 1/05. Variables-Data-Expressions-and-Constants/VariablesDataExpressionsConstants/ClassSize/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassSize
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main()
+        {
+            double initialSizeWidth = 54.5;
+            double initialSizeHeight = 20.3;
+
+            var initialSize = new Size(initialSizeWidth, initialSizeHeight);
+            Console.WriteLine(initialSize);
+
+            var rotationAngle = 30;
+            var rotatedSize = Size.GetRotatedSize(initialSize, rotationAngle);
+            Console.WriteLine(rotatedSize);
+        }
+    }
+}
