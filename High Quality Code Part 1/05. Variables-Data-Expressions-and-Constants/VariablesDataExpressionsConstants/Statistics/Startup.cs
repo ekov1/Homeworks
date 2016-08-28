315c9cc5217a97e560660a00219a011e499abbc4
lllevski <mitkostoikov1988@gmail.com>
2016-08-28 15:25:35 +0300

    update

---
+++ High Quality Code Part 1/05. Variables-Data-Expressions-and-Constants/VariablesDataExpressionsConstants/Statistics/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace Statistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main()
+        {
+            var statisctic = new double[] { 3.526, 5.67, 4.404, 50.30, 66.66, 69.609, 0.104, -45.5005043 };
+            var statViewer = new StatisticsViewer();
+
+            statViewer.PrintStatistics(statisctic);
+        }
+    }
+}
---
+++ High Quality Code Part 1/05. Variables-Data-Expressions-and-Constants/VariablesDataExpressionsConstants/Statistics/StatisticsViewer.cs
@@ -0,0 +1,80 @@
+﻿namespace Statistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StatisticsViewer
+    {
+        private const char ConsoleSeparatorCharacter = '=';
+        private const int ConsoleSeparatorRepearCount = 47;
+
+        public double GetMaxValue(double[] statistics)
+        {
+            double currentMax = statistics[0];
+
+            for (int i = 1; i < statistics.Length; i++)
+            {
+                if (statistics[i] > currentMax)
+                {
+                    currentMax = statistics[i];
+                }
+            }
+
+            return currentMax;
+        }
+
+        public double GetMinValue(double[] statistics)
+        {
+            double currentMin = statistics[0];
+
+            for (int i = 1; i < statistics.Length; i++)
+            {
+                if (statistics[i] < currentMin)
+                {
+                    currentMin = statistics[i];
+                }
+            }
+
+            return currentMin;
+        }
+
+        public double GetSum(double[] statisctics)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < statisctics.Length; i++)
+            {
+                sum += statisctics[i];
+            }
+
+            return sum;
+        }
+
+        public double GetAverageValue(double[] statisctics)
+        {
+            double average = GetSum(statisctics) / statisctics.Length;
+
+            return average;
+        }
+
+        public void PrintStatistics(double[] statisctics)
+        {
+            string maxValue = GetMaxValue(statisctics).ToString("0.00");
+            string minValue = GetMinValue(statisctics).ToString("0.00");
+            string averageValue = GetAverageValue(statisctics).ToString("0.00");
+            var builder = new StringBuilder();
+
+            builder.AppendLine("The given statistics show the following results");
+            builder.AppendLine(new string(ConsoleSeparatorCharacter, ConsoleSeparatorRepearCount));
+
+            builder.AppendLine($"Maximum value is around {maxValue} ");
+            builder.AppendLine($"Minimum value is around {minValue} ");
+            builder.AppendLine($"The average value is around {averageValue} ");
+
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
