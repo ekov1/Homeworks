1590fca76aa6c7481d2e110203be448d546f5c49
lllevski <mitkostoikov1988@gmail.com>
2016-09-04 01:08:06 +0300

    update

--- JavaScript OOP/02. Functions-and-Function-Expressions/tasks/task-2.js
+++ JavaScript OOP/02. Functions-and-Function-Expressions/tasks/task-2.js
@@ -6,8 +6,36 @@
 		3) it must throw an Error if any of the range params is missing
 */
 
-function findPrimes() {
-	
+function findPrimes(...values) {
+	let [startRange, endRange] = values.map(Number);
+	let areConvertibleToNumber = isFinite(startRange) && isFinite(endRange);
+
+	if (arguments.length < 2 || !areConvertibleToNumber) {
+		throw new Error('Cannot parse numbers correctly!');
+	}
+
+	let primeNumbers = [];
+
+	for (let i = startRange; i <= endRange; i += 1) {
+		if (isPrime(i)) {
+			primeNumbers.push(i);
+		}
+	}
+
+	function isPrime(number) {
+		if (number < 2) {
+			return false;
+		}
+		for (var i = 2; i < number; i++) {
+			if (number % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	return primeNumbers;
 }
+
 
 module.exports = findPrimes;
