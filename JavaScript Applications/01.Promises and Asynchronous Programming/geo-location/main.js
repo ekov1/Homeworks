37250ff740a6c9c6a50f9170e3331e6141765ffa
lllevski <mitkostoikov1988@gmail.com>
2016-09-16 16:14:19 +0300

    update

--- JavaScript Applications/01.Promises and Asynchronous Programming/geo-location/main.js
+++ JavaScript Applications/01.Promises and Asynchronous Programming/geo-location/main.js
@@ -11,12 +11,14 @@
     function drawImage(position) {
         let lat = position.coords.latitude,
             long = position.coords.longitude,
-            container = document.getElementById('container');
+            container = document.getElementById('container'),
+            image = document.createElement('img');
 
         let imageSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
             lat + ',' + long + '&zoom=18&size=500x500&sensor=false';
 
-        container.src = imageSrc;
+        image.src = imageSrc;
+        container.appendChild(image);
     }
 
     getLocation
