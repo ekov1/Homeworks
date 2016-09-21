5f6163a679eec2b50af28500d5063d0249c2e4e3
lllevski <mitkostoikov1988@gmail.com>
2016-09-22 01:24:25 +0300

    update

--- High Quality Code Part 2/03. Refactoring/MatrixWalk/Matrix.cs
+++ High Quality Code Part 2/03. Refactoring/MatrixWalk/Matrix.cs
@@ -6,13 +6,14 @@
     {
         public static void Main()
         {
-            //Console.WriteLine( "Enter a positive number " );
-            //string input = Console.ReadLine(  );
+            //Console.WriteLine("Enter a positive number ");
+            //string input = Console.ReadLine();
             //int n = 0;
+
             //while ( !int.TryParse( input, out n ) || n < 0 || n > 100 )
             //{
-            //    Console.WriteLine( "You haven't entered a correct positive number" );
-            //    input = Console.ReadLine(  );
+            //    Console.WriteLine("You haven't entered a correct positive number");
+            //    input = Console.ReadLine();
             //}
 
             int n = 3;
@@ -26,12 +27,11 @@
 
             while (true)
             {
-                //malko e kofti tova uslovie, no break-a raboti 100% : )
                 matrica[i, j] = k;
 
                 if (!proverka(matrica, i, j))
                 {
-                    // prekusvame ako sme se zadunili
+                    PrintMatrixSteps(matrica);
                     break;
                 }
 
@@ -47,54 +47,17 @@
                 j += dy;
                 k++;
             }
+        }
 
-            for (int p = 0; p < n; p++)
+        private static void PrintMatrixSteps(int[,] matrix)
+        {
+            int rowsNumber = matrix.GetLength(0);
+
+            for (int row = 0; row < rowsNumber; row++)
             {
-                for (int q = 0; q < n; q++)
+                for (int col = 0; col < rowsNumber; col++)
                 {
-                    Console.Write("{0,3}", matrica[p, q]);
-                }
-
-                Console.WriteLine();
-            }
-
-            find_cell(matrica, out i, out j);
-
-            //if (i != 0 && j != 0)
-            //{ // taka go napravih, zashtoto funkciqta ne mi davashe da ne si definiram out parametrite
-            //    dx = 1;
-            //    dy = 1;
-
-            //    while (true)
-            //    {
-            //        //malko e kofti tova uslovie, no break-a raboti 100% : )
-            //        matrica[i, j] = k;
-
-            //        if (!proverka(matrica, i, j))
-            //        {
-            //            // prekusvame ako sme se zadunili
-            //            break;
-            //        }
-
-            //        if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0)
-            //        {
-            //            while (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0)
-            //            {
-            //                change(ref dx, ref dy);
-            //            }
-            //        }
-
-            //        i += dx;
-            //        j += dy;
-            //        k++;
-            //    }
-            //}
-
-            for (int pp = 0; pp < n; pp++)
-            {
-                for (int qq = 0; qq < n; qq++)
-                {
-                    Console.Write("{0,3}", matrica[pp, qq]);
+                    Console.Write("{0,3}", matrix[row, col]);
                 }
 
                 Console.WriteLine();
@@ -154,15 +117,17 @@
             return false;
         }
 
-        private static void find_cell(int[,] arr, out int x, out int y)
+        private static void find_cell(int[,] matrix, out int x, out int y)
         {
+            int rowsNumber = matrix.GetLength(0);
             x = 0;
             y = 0;
-            for (int i = 0; i < arr.GetLength(0); i++)
+
+            for (int i = 0; i < rowsNumber; i++)
             {
-                for (int j = 0; j < arr.GetLength(0); j++)
+                for (int j = 0; j < rowsNumber; j++)
                 {
-                    if (arr[i, j] == 0)
+                    if (matrix[i, j] == 0)
                     {
                         x = i;
                         y = j;
