af8cd45704f062d526cbc7680e1310e15732bdbc
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-14 22:13:15 +0300

    update tree traversals

---
+++ Data-Structures-and-Algorithms/07. Trees-and-Traversals/Tree-and-Treelike-Structures/01.RedRidingHood/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.RedRidingHood
+{
+	// Works for 36/100 needs  optimization
+    public class Startup
+    {
+        public static void Main()
+        {
+            var destinationsCount = int.Parse(Console.ReadLine());
+            var connectionsCount = destinationsCount - 1;
+
+            var connections = new LinkedList<int>[destinationsCount + 1];
+            var nodeValues = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < connectionsCount; i++)
+            {
+                var input = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+                if (connections[input[0]] == null)
+                {
+                    connections[input[0]] = new LinkedList<int>();
+                }
+
+                connections[input[0]].AddLast(input[1]);
+
+                if (connections[input[1]] == null)
+                {
+                    connections[input[1]] = new LinkedList<int>();
+                }
+
+                connections[input[1]].AddLast(input[0]);
+            }
+
+            var graph = new Graph(connections, nodeValues);
+
+            graph.FindMostMoney();
+        }
+    }
+
+    public class Graph
+    {
+        private LinkedList<int>[] nodes;
+        private HashSet<int> visited;
+        private int maxValue;
+        private int[] values;
+
+        public Graph(LinkedList<int>[] nodes, int[] values)
+        {
+            this.values = values;
+            this.visited = new HashSet<int>();
+            this.nodes = nodes;
+            this.maxValue = 0;
+        }
+
+        public void FindMostMoney()
+        {
+            for (int i = 1; i < this.nodes.Length; i++)
+            {
+                this.Bfs(i);
+            }
+
+            Console.WriteLine(this.maxValue);
+        }
+
+        public void Bfs(int node)
+        {
+            var queue = new Queue<int>();
+            var moneyFound = new Queue<int>();
+            this.visited = new HashSet<int>();
+
+            queue.Enqueue(node);
+            moneyFound.Enqueue(this.values[node - 1]);
+            this.visited.Add(node);
+
+            while (queue.Count != 0)
+            {
+                var currentNode = queue.Dequeue();
+                var currentMoney = moneyFound.Dequeue();
+
+                if (currentMoney > this.maxValue)
+                {
+                    maxValue = currentMoney;
+                }
+
+                foreach (var item in this.nodes[currentNode])
+                {
+                    if (!this.visited.Contains(item))
+                    {
+                        this.visited.Add(item);
+                        queue.Enqueue(item);
+                        moneyFound.Enqueue(currentMoney + this.values[item - 1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/07. Trees-and-Traversals/Tree-and-Treelike-Structures/02.GraphDiameter/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.GraphDiameter
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var nodesAndEdges = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var connections = new int[nodesAndEdges[0] + 1][];
+
+            for (int i = 0; i < nodesAndEdges[1]; i++)
+            {
+                var input = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+                if (connections[input[0]] == null)
+                {
+                    connections[input[0]] = new int[nodesAndEdges[0] + 1];
+                }
+
+                connections[input[0]][input[1]] = 1;
+
+                if (connections[input[1]] == null)
+                {
+                    connections[input[1]] = new int[nodesAndEdges[0] + 1];
+                }
+
+                connections[input[1]][input[0]] = 1;
+            }
+
+            var graph = new Graph(connections);
+
+            graph.FindDiameter();
+        }
+    }
+
+    public class Graph
+    {
+        private int[][] nodes;
+        private HashSet<int> visited;
+        private int maxValue;
+
+        public Graph(int[][] nodes)
+        {
+            this.visited = new HashSet<int>();
+            this.nodes = nodes;
+            this.maxValue = 0;
+        }
+
+        public void FindDiameter()
+        {
+            for (int i = 1; i < this.nodes.Length; i++)
+            {
+                this.Bfs(i);
+            }
+
+            Console.WriteLine(maxValue);
+        }
+
+        public void Bfs(int node)
+        {
+            var queue = new Queue<int>();
+            var travelledWay = new Queue<int>();
+            this.visited = new HashSet<int>();
+
+            queue.Enqueue(node);
+            travelledWay.Enqueue(0);
+            this.visited.Add(node);
+
+            while (queue.Count != 0)
+            {
+                var currentNode = queue.Dequeue();
+                var currentDestination = travelledWay.Dequeue();
+                if (currentDestination > maxValue)
+                {
+                    maxValue = currentDestination;
+                }
+
+                for (int i = 0; i < this.nodes.Length; i++)
+                {
+                    if (this.nodes[currentNode]?[i] > 0 && !this.visited.Contains(i))
+                    {
+                        this.visited.Add(i);
+                        queue.Enqueue(i);
+                        travelledWay.Enqueue(currentDestination + 1);
+                    }
+                }
+            }
+        }
+    }
+}
