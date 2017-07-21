d3e1c78c2a7c5c93f8578ab70a2718b277a21eb3
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-21 20:20:17 +0300

    update dynamic programming tasks

---
+++ Data-Structures-and-Algorithms/14. Dynamic-Programming/DynamicProgrammingNew/SuperMarioTest/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace SuperMarioTest
+{
+    public class IndexedTree
+    {
+        private long[] tree;
+        private int lastIndex;
+        private int arraySize;
+        private int leafIndex;
+
+        public IndexedTree(int elementsCount)
+        {
+            this.leafIndex = 1;
+
+            while (elementsCount > this.leafIndex)
+            {
+                this.leafIndex *= 2;
+            }
+
+            this.tree = new long[this.leafIndex * 2];
+
+            for (int i = 0; i < tree.Length; i++)
+            {
+                this.tree[i] = long.MaxValue;
+            }
+
+            this.lastIndex = 0;
+            this.arraySize = elementsCount;
+        }
+
+        public long Top
+        {
+            get
+            {
+                return this.tree[1];
+            }
+        }
+
+        public void Push(long value)
+        {
+            var indexToInsert = this.leafIndex + this.lastIndex;
+            ++this.lastIndex;
+
+            if (this.lastIndex == this.arraySize)
+            {
+                this.lastIndex = 0;
+            }
+
+            this.tree[indexToInsert] = value;
+
+            while (indexToInsert > 1)
+            {
+                tree[indexToInsert / 2] = Math.Min(tree[indexToInsert], tree[indexToInsert ^ 1]);
+                indexToInsert /= 2;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            var strs = Console.ReadLine().Split(' ');
+            var n = int.Parse(strs[0]);
+            var k = int.Parse(strs[1]);
+
+            if (n < k)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            strs = Console.ReadLine().Split(' ');
+            var f = long.Parse(strs[0]);
+            var a = int.Parse(strs[1]);
+            var b = int.Parse(strs[2]);
+            var m = int.Parse(strs[3]);
+
+            var tree = new IndexedTree(k);
+            tree.Push(f);
+
+            for (int i = 1; i < k; i++)
+            {
+                f = (f * a + b) % m;
+                tree.Push(f);
+            }
+
+            for (int i = k; i < n; i++)
+            {
+                f = (f * a + b) % m;
+                var currentValue = tree.Top + f;
+                tree.Push(currentValue);
+            }
+
+            Console.WriteLine(tree.Top);
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/14. Dynamic-Programming/DynamicProgrammingNew/SuperMarioWorkshop/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace SuperMarioWorkshop
+{
+    public class Startup
+    {
+        // generating cell formula: (Ci * A + B) % M
+        public static void Main()
+        {
+            var nk = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var fabm = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = nk[0];
+            var k = nk[1];
+
+            var f = fabm[0];
+            var a = fabm[1];
+            var b = fabm[2];
+            var m = fabm[3];
+
+            var prices = new long[n + 1];
+            prices[0] = f;
+
+            for (int i = 1; i < n; i++)
+            {
+                prices[i] = (prices[i - 1] * a + b) % m;
+            }
+
+            for (int i = k; i <= n; i++)
+            {
+                var minResult = long.MaxValue;
+
+                for (int j = i - k; j < i; j++)
+                {
+                    var currentElement = prices[j];
+
+                    if (currentElement < minResult)
+                    {
+                        minResult = currentElement;
+                    }
+                }
+
+                prices[i] += minResult;
+            }
+
+            Console.WriteLine(prices[n]);
+        }
+    }
+}
